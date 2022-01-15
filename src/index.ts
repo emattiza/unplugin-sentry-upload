d152a8564246ac4921937dcadcea5966380e4bd8
Evan Mattiza <emattiza@gmail.com>
2022-01-15 03:29:09 +0000

    added esbuild output module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import { createUnplugin } from 'unplugin'
 import { Options } from './types'
 
-export default createUnplugin<Options>(options => ({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default createUnplugin<Options>(_options => ({
   name: 'unplugin-sentry-upload',
-  transformInclude(id) {
-    return id.endsWith('main.ts')
-  },
-  transform(code) {
-    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
-  },
 }))
